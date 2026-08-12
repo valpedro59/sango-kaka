44ccfa2df2317956225bd6dd063bcfdb7a87b670
GAEL-CODE-dev <gaelyangu@gmail.com>
2026-08-12 19:04:28 +0100

    setup tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-center mb-3 text-3xl">
+      <h1 className="text-3xl font-bold text-center text-blue-600">
         Bonjour et Bienvuenue sur votre application
       </h1>
       <p className="text-center">Veuillez commencer vos taches</p>
