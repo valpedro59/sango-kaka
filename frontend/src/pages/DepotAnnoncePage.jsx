de1a8272cae51e2e6b94b7cc1c3031d70eb449e5
GLISH-prog <shekinahkitsoro@gmail.com>
2026-08-14 09:12:57 +0100

    feat(frontend): Ajout du formulaire dépot d'annonce

---
+++ frontend/src/pages/DepotAnnoncePage.jsx
@@ -0,0 +1,10 @@
+import DepotAnnonceForm from "../components/DepotAnnonceForm";
+function DepotAnnoncePage() {
+  return (
+    <main className="min-h-screen bg-white">
+      <DepotAnnonceForm />
+    </main>
+  );
+}
+
+export default DepotAnnoncePage;
