673d94047febd4af93137b164b435033124d7836
GAEL-CODE-dev <gaelyangu@gmail.com>
2026-08-28 12:16:24 +0100

    fix: harmonisation du port de l'API backend sur 3001

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./src/app.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Serveur lancé sur http://localhost:${PORT}`);
