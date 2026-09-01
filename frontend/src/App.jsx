2cdebcc4a6de8ac390383de1c6b14997123448cb
GAEL-CODE-dev <gaelyangu@gmail.com>
2026-09-01 11:47:31 +0100

    chore: suppression de App.css (doublon de l'import Tailwind)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 
 import MainLayout from "./layouts/MainLayout";
 import AccueilPage from "./pages/AccueilPage";
